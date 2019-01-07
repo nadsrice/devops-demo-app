9e4d435ddadbf02eacc96674925db414fcf6d2da
Ronald <nadsrice@gmail.com>
2019-01-07 21:26:21 +0800

    Update devops-demo-1.0.sql

--- data/devops-demo-1.0.sql
+++ data/devops-demo-1.0.sql
@@ -1,5 +1,5 @@
 CREATE TABLE dbversion (id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,version VARCHAR(30) NOT NULL);
-INSERT INTO dbversion (version) VALUES ('1.0');
+INSERT INTO dbversion tbl_name (version) VALUES ('1.0');
 CREATE USER 'root'@'%' IDENTIFIED BY 'root';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
 flush PRIVILEGES;
